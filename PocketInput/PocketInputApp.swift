2ac6cb0e66f0c51c2e042637ecfe1ae67783d7e1
EddieIpeace <33387844+EddieIpeace@users.noreply.github.com>
2026-07-15 21:17:01 +0800

    Initial Commit

---
+++ PocketInput/PocketInputApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketInputApp.swift
+//  PocketInput
+//
+//  Created by 闵统 on 2026/7/15.
+//
+
+import SwiftUI
+
+@main
+struct PocketInputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
